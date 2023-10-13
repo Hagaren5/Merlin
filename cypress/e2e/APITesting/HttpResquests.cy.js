f80287c6254fd906d8697ca989817317f0da889b
Oriol Sepúlveda <oriolsm93@gmail.com>
2023-10-13 19:18:24 +0200

    Question3 done

--- cypress/e2e/APITesting/HttpResquests.cy.js
+++ cypress/e2e/APITesting/HttpResquests.cy.js
@@ -1,33 +1,80 @@
-import '../../fixtures/users.json'
+import '../../fixtures/users.json';
+// import '../../support/functions'
 
-describe('Create User with Http Request' , () => {
+const POSTendPoint = 'https://petstore.swagger.io/v2/user/createWithArray'
+const GETUserendPoint = 'https://petstore.swagger.io/v2/user'
+const GETPETendPoint = 'https://petstore.swagger.io/v2/pet/findByStatus?status=sold'
+const newUser = [ {
+  id: 10,
+  username: 'alpha',
+  firstName: 'John',
+  lastName: 'James',
+  email: 'john@email.com',
+  password: '12345',
+  phone: '12345',
+  userStatus: 1,
+}];
+const userNames = newUser.map((user) => user.username)
 
-    it('Create the user', () => {
+export class PetCounter {
+  constructor(pets) {
+    this.pets = pets;
+  }
 
-        const requestBody = {
-            id: 0,
-            username: "Oriol",
-            firstName: "string",
-            lastName: "string",
-            email: "string",
-            password: "string",
-            phone: "string",
-            userStatus: 0
-        }
+  sameNamePetCounter() {
+    const nameCounts = {};
 
+    this.pets.forEach((pet) => {
+      const name = pet.name;
 
-        cy.request({
-            method: 'POST',
-            url: 'https://petstore.swagger.io/v2/user/createWithList',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-api-key': 'special_key', // Add your API key here
-              },
-            body: requestBody
-        }).then((response) => {
+      if (nameCounts[name]) {
+        nameCounts[name]++;
+      } else {
+        nameCounts[name] = 1;
+      }
+    });
 
-            expect(response.body.code).to.eq(201)
-            }
-        )
+    return nameCounts;
+  }
+}
+
+describe('API Creating user', () => {
+  it('Create user', () => {
+    cy.request({
+      method: 'POST',
+      url: POSTendPoint,
+      body: newUser,
+    }).then((response) => {
+      console.log(response);
+      expect(response.status).to.eq(200)
     })
+  })
+})
+
+describe('GET', () => {
+  it('User by Username', () => {
+    cy.request(
+      'GET',
+      `${GETUserendPoint}/${userNames}`
+    ).then((response) => {
+      console.log(response.body)
+    })
+  })
+})
+
+describe('GET', () => {
+  it.only('Pet by status', () => {
+    cy.request(
+      'GET',
+      GETPETendPoint,
+    ).then((response) => {
+        console.log(response.body)
+        const petCounter = new PetCounter(response.body);
+        const resultado = petCounter.sameNamePetCounter()
+
+        for(const name in resultado){
+          console.log(`Name: ${name}: ${resultado[name]} `)
+        }
+      })
+  })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,20 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('POSTCall', (selector) => {
+    cy.request({
+        method: 'POST',
+        url: 'https://petstore.swagger.io/v2/user/createWithList',
+        body: {
+            id: 0,
+            username: 'Oriol',
+            firstName: 'string',
+            lastName: 'string',
+            email: 'string',
+            password: 'string',
+            phone: 'string',
+            userStatus: 0,
+          },
+      }).then((response) => {
+        expect(response.body.code).to.eq(201);
+      });
+})
